3c6af73c73985e3ce2ff50981a719f6bc331189d
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-09-23 12:51:05 -0500

    added create and user_params methods

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_09_22_172840) do
 
   create_table "users", force: :cascade do |t|
-    t.string "usarname"
+    t.string "username"
     t.string "email"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
